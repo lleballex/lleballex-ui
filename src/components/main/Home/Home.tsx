88624b069526214c5c5db3b3c842893702e90008
lleballex <lleballex@yandex.ru>
2023-11-27 18:40:29 +0300

    Update styles

--- src/components/main/Home/Home.tsx
+++ src/components/main/Home/Home.tsx
@@ -1,26 +1,26 @@
-import HomeModal from './HomeModal'
+// import HomeModal from './HomeModal'
 import HomeButton from './HomeButton'
-import HomeInput from './HomeInput'
-import HomeSelect from './HomeSelect'
-import HomeSwitch from './HomeSwitch'
-import HomeRadioCheckbox from './HomeRadioCheckbox'
-import HomeDate from './HomeDate'
-import HomeSlider from './HomeSlider'
-import HomeToast from './HomeToast'
+// import HomeInput from './HomeInput'
+// import HomeSelect from './HomeSelect'
+// import HomeSwitch from './HomeSwitch'
+// import HomeRadioCheckbox from './HomeRadioCheckbox'
+// import HomeDate from './HomeDate'
+// import HomeSlider from './HomeSlider'
+// import HomeToast from './HomeToast'
 import styles from './Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <HomeModal />
+      {/* <HomeModal /> */}
       <HomeButton />
-      <HomeInput />
-      <HomeSelect />
-      <HomeSwitch />
-      <HomeRadioCheckbox />
-      <HomeDate />
-      <HomeSlider />
-      <HomeToast />
+      {/* <HomeInput /> */}
+      {/* <HomeSelect /> */}
+      {/* <HomeSwitch /> */}
+      {/* <HomeRadioCheckbox /> */}
+      {/* <HomeDate /> */}
+      {/* <HomeSlider /> */}
+      {/* <HomeToast /> */}
       *filters* *password input, textarea* *forms*
     </div>
   )
