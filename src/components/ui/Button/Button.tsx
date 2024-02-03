388d888db7d4faa0d14fac958eabc0d1c6d4291f
lleballex <lleballex@yandex.ru>
2024-02-03 14:06:42 +0300

    Add link props to Button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,13 +1,17 @@
-import { ButtonHTMLAttributes, MouseEventHandler, ReactNode } from 'react'
+import { HTMLAttributeAnchorTarget, ReactNode } from 'react'
+import { Url } from 'url'
 import classNames from 'classnames'
+import Link from 'next/link'
 import MountTransition from '@/components/ui/MountTransition'
 import BaseButton, { BaseButtonProps } from '@/components/ui/BaseButton'
 import Spinner from '@/components/ui/Spinner'
 import styles from './Button.module.scss'
 
-interface Props extends BaseButtonProps {
+interface Props extends Omit<BaseButtonProps, 'disabled'> {
   className?: string
   type?: 'primary' | 'secondary' | 'text'
+  href?: Url | string
+  target?: HTMLAttributeAnchorTarget
   fullWidth?: boolean
   loading?: boolean
   children?: ReactNode
@@ -16,6 +20,8 @@ interface Props extends BaseButtonProps {
 export default function Button({
   className,
   type = 'primary',
+  href,
+  target,
   fullWidth,
   loading,
   children,
@@ -24,18 +30,30 @@ export default function Button({
   // TODO: ref
   // TODO: link
 
-  return (
-    <BaseButton
-      {...baseButtonProps}
-      className={classNames(styles.button, styles[type], className, {
-        [styles.full]: fullWidth,
-      })}
-      disabled={loading}
-    >
-      {children}
-      <MountTransition gap horizontal>
-        {loading && <Spinner />}
-      </MountTransition>
-    </BaseButton>
-  )
+  if (href) {
+    return (
+      <Link
+        className={classNames(styles.link, styles[type], className)}
+        href={href}
+        target={target}
+      >
+        {children}
+      </Link>
+    )
+  } else {
+    return (
+      <BaseButton
+        {...baseButtonProps}
+        className={classNames(styles.button, styles[type], className, {
+          [styles.full]: fullWidth,
+        })}
+        disabled={loading}
+      >
+        {children}
+        <MountTransition gap horizontal>
+          {loading && <Spinner />}
+        </MountTransition>
+      </BaseButton>
+    )
+  }
 }
